ec5769e87dbe6a3f136ae7bfe4a639ebb837e819
Diddle1 <159637379+Diddle1@users.noreply.github.com>
2025-03-03 10:30:38 -0500

    LostandFound.py

--- LostandFound.py
+++ LostandFound.py
@@ -1,7 +1,9 @@
 # start_screen.py
 from colorama import Fore, Style, init
-init(autoreset=True)
 import search  # Importing the search module
+
+# Initialize colorama for colored text output
+init(autoreset=True)
 
 # Looping the main menu
 while True:
@@ -32,25 +34,37 @@ while True:
         target = input(Fore.GREEN + "Enter the email/target information to search for: ")
         file_or_folder = input(Fore.GREEN + "Enter the file or folder to search in: ")
         output_file = input(Fore.GREEN + "Enter the output file name: ")
+        num_threads = input(Fore.GREEN + "Enter the number of threads (Default: 4): ")
+
         if target == '99' or file_or_folder == '99' or output_file == '99':
             continue
-        search.search_files(target, file_or_folder, output_file)
-    elif choice == '2':
-        print(Fore.CYAN + r"""Relative Path Example:
 
-file_or_folder input: example.txt
-This will search for example.txt in the same directory where your Python script is running.
+        num_threads = int(num_threads) if num_threads.isdigit() else 4  # Default to 4 threads
+        search.search_files(target, file_or_folder, output_file, num_threads)
+
+    elif choice == '2':
+        print(Fore.CYAN + r"""How to Use:
+
+Relative Path Example:
+    file_or_folder input: example.txt
+    - This will search for example.txt in the same directory as your script.
 
 Absolute Path Example:
+    file_or_folder input: C:\Users\YourName\Documents\example.txt (Windows)
+    /home/username/Documents/example.txt (Linux/Mac)
+    - This will search for the file in the specified directory.
 
-file_or_folder input: C:\Users\YourName\Documents\example.txt (Windows) or /home/username/Documents/example.txt (Linux/Mac)
-This will search for example.txt in the specified directory.
+Multithreading:
+    - You can specify the number of threads to speed up the search.
+    - Default is 4 threads. More threads = faster search but higher CPU usage.
     """)
         help_input = input(Fore.RED + "Press 99 to return to the main menu: ")
         if help_input == '99':
             continue
+
     elif choice == '99':
         print(Fore.RED + "Exiting!")
         break
+
     else:
-        print("Invalid choice. Please select 1, 2, or 99 to exit.")
+        print(Fore.RED + "Invalid choice. Please select 1, 2, or 99 to exit.")
